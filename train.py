f8558294208f03da813f834e9e7c6927a140432c
Adithya Maurya K R <135137252+adithyamauryakr@users.noreply.github.com>
2025-06-06 10:30:00 +0530

    r2score error fix

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from tqdm import tqdm
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
 import torch
 from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
